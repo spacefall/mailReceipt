f6a9c00ee8d8772225e73a25d64348519f2671f9
spacefall <lucarichu@outlook.it>
2025-03-04 14:41:14 +0100

    refactor: slight cleanup

--- delete.go
+++ delete.go
@@ -8,18 +8,21 @@ import (
 
 func removeTracking(c *fiber.Ctx) error {
 	id := c.Params("uuid")
-	if id == "" {
-		return c.Status(400).SendString("Missing tracking id")
-	}
+
+	// Check if uuid is valid
 	if err := uuid.Validate(id); err != nil {
 		return c.Status(400).SendString("Invalid tracking id")
 	}
+
+	// Delete the row corresponding to the uuid
 	res, err := dbpool.Exec(context.Background(), "DELETE FROM mail_receipts WHERE id = $1", id)
 	if err != nil {
 		return err
 	}
+
+	// If no rows were affected, the uuid doesn't exist
 	if res.RowsAffected() == 0 {
 		return c.Status(404).SendString("Tracking id not found")
 	}
-	return c.SendString("Tracking id removed")
+	return c.SendStatus(200)
 }
--- pixelTrack.go
+++ pixelTrack.go
@@ -12,6 +12,7 @@ import (
 var img = []byte{71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 128, 0, 0, 255, 255, 255, 0, 0, 0, 33, 249, 4, 1, 0, 0, 0, 0, 44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59}
 
 func pixelTrack(c *fiber.Ctx) error {
+	// Add request to logs asynchronously
 	go func(id string, ip string) {
 		// Log info
 		log.Printf("New Request\nID: %s\nIP: %s\n", id, ip)
